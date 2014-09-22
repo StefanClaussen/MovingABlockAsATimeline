2f192e37429f894ebb7f1dc5090e58cc2600f83d
StefanClausen <stefanclaussen@me.com>
2014-09-22 15:10:02 +0100

    Timer countdowns on a view.

--- TimerPlaying/ViewController.m
+++ TimerPlaying/ViewController.m
@@ -14,10 +14,56 @@
 
 @implementation ViewController
 
+
+
+//@interface ViewController()
+//{
+//    UILabel *progress;
+//    NSTimer *timer;
+//    int currMinute;
+//    int currSeconds;
+//}
+//@end
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    progress=[[UILabel alloc] initWithFrame:CGRectMake(80, 15, 100, 50)];
+    progress.textColor=[UIColor redColor];
+    [progress setText:@"Time : 3:00"];
+    progress.backgroundColor=[UIColor clearColor];
+    [self.view addSubview:progress];
+    currMinute=3;
+    currSeconds=00;
+    [self start];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(void)start
+{
+    timer=[NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(timerFired) userInfo:nil repeats:YES];
+    
+}
+-(void)timerFired
+{
+    if((currMinute>0 || currSeconds>=0) && currMinute>=0)
+    {
+        if(currSeconds==0)
+        {
+            currMinute -= 1;
+            currSeconds=59;
+        }
+        else if(currSeconds>0)
+        {
+            currSeconds -= 1;
+        }
+        if(currMinute > -1)
+            [progress setText:[NSString stringWithFormat:@"%@%d%@%02d",@"Time : ",currMinute,@":",currSeconds]];
+    }
+    else
+    {
+        [timer invalidate];
+    }
 }
 
 - (void)didReceiveMemoryWarning
